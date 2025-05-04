9b74c3f1cc1abdd3580eeb05437b188814d3aa07
kkyuheak <gyuheak@gmail.com>
2025-05-04 21:37:25 +0900

    feat: 즐겨찾기 코인리스트 한글이름 추가

--- src/app/[user]/favorite/page.tsx
+++ src/app/[user]/favorite/page.tsx
@@ -6,6 +6,8 @@ import { getDetailTicker } from "@/utils/coin/getDetailTicker";
 import { useQuery } from "@tanstack/react-query";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useEffect, useState } from "react";
+import { getCoinName } from "@/utils/coin/getCoinName";
+import CoinListSkeleton from "@/components/coin/CoinListSkeleton";
 
 const UserFavoritePage = () => {
   const getUserFavoriteCoin = async () => {
@@ -39,10 +41,14 @@ const UserFavoritePage = () => {
     }
   }, [favoriteCoinData, selectedTab]);
 
-  return (
-    <div>
-      <h1 className="text-[28px] font-bold my-[20px]">즐겨찾기</h1>
+  // 코인 이름
+  const { data: coinName } = useQuery<AllCoinNameType[]>({
+    queryKey: ["coinName"],
+    queryFn: getCoinName,
+  });
 
+  return (
+    <>
       <Tabs
         defaultValue="KRW"
         className="w-[400px]"
@@ -57,33 +63,40 @@ const UserFavoritePage = () => {
       <table className="w-full m-auto border-t border-[#d8d8d8] mt-5">
         <thead className="h-[42px]">
           <tr>
-            <th className="pl-1"></th>
-            <th className="text-left pl-2">코인</th>
-            <th className="">현재가</th>
+            <th className="text-left pl-3">코인</th>
+            <th>현재가</th>
             <th className=" w-[100px]">전일대비</th>
-            <th className="">거래량(24H)</th>
-            <th className="">거래대금(24H)</th>
+            <th>거래량(24H)</th>
+            <th>거래대금(24H)</th>
           </tr>
           <tr className="border-b border-[#d8d8d8]"></tr>
         </thead>
         <tbody>
-          {filterdFavoriteCoinData?.map((coin) => {
-            return (
-              <CoinListBox
-                key={coin.market}
-                coinName={coin.market}
-                market={coin.market}
-                price={coin.trade_price}
-                changeRate={coin.signed_change_rate}
-                accTradeVolume24h={coin.acc_trade_volume_24h}
-                accTradePrice24h={coin.acc_trade_price_24h}
-                tabName={selectedTab}
-              />
-            );
-          })}
+          {filterdFavoriteCoinData && coinName
+            ? filterdFavoriteCoinData.map((coin) => {
+                const kr_name = coinName?.find(
+                  (nameData) => nameData.market === coin.market
+                );
+                return (
+                  <CoinListBox
+                    key={coin.market}
+                    coinName={kr_name?.korean_name ?? ""}
+                    market={coin.market}
+                    price={coin.trade_price}
+                    changeRate={coin.signed_change_rate}
+                    accTradeVolume24h={coin.acc_trade_volume_24h}
+                    accTradePrice24h={coin.acc_trade_price_24h}
+                    tabName={selectedTab}
+                    isLoggedIn={null}
+                  />
+                );
+              })
+            : Array.from({ length: 10 }).map((_, i) => (
+                <CoinListSkeleton key={i} />
+              ))}
         </tbody>
       </table>
-    </div>
+    </>
   );
 };
 
