7e7a10b6189701791af6956f40865d81408a8216
kkyuheak <gyuheak@gmail.com>
2025-05-26 22:24:38 +0900

    feat: 글 등록하기 제목,내용,해시태그 input 에러핸들링

--- src/components/common/input/PostingInput.tsx
+++ src/components/common/input/PostingInput.tsx
@@ -1,19 +1,46 @@
+import { PostingFormValues } from "@/components/community/PostingForm";
+import { FieldErrors, Path, UseFormRegister } from "react-hook-form";
 import { twMerge } from "tailwind-merge";
 
 interface PostingInputProps {
   placeholder: string;
   css?: string;
+  register: UseFormRegister<PostingFormValues>;
+  label: Path<PostingFormValues>;
+  errors: FieldErrors<PostingFormValues>;
 }
 
-const PostingInput = ({ placeholder, css }: PostingInputProps) => {
+const PostingInput = ({
+  placeholder,
+  css,
+  register,
+  label,
+  errors,
+}: PostingInputProps) => {
+  const errorInputMessage =
+    label === "title"
+      ? "제목은 필수로 입력해주세요"
+      : "해시태그는 필수로 작성해주세요";
+
   return (
-    <input
-      placeholder={placeholder}
-      className={twMerge(
-        "w-[448px] h-[56px] rounded-[12px] bg-[#f2f5f2] px-4 outline-none block",
-        css
+    <div>
+      <label htmlFor={label} className="mb-1 font-bold block">
+        {label === "title" ? "제목" : "해시태그"}
+      </label>
+      <input
+        placeholder={placeholder}
+        className={twMerge(
+          "w-[448px] h-[56px] rounded-[12px] bg-[#f2f5f2] px-4 outline-none block",
+          css,
+          errors[label] ? "border border-red-500" : ""
+        )}
+        id={label}
+        {...register(label, { required: errorInputMessage })}
+      />
+      {errors[label] && (
+        <p className="text-red-500 text-[12px]">{errors[label].message}</p>
       )}
-    />
+    </div>
   );
 };
 
--- src/components/community/PostingForm.tsx
+++ src/components/community/PostingForm.tsx
@@ -1,17 +1,63 @@
+"use client";
+
+import { twMerge } from "tailwind-merge";
 import SimpleButton from "../common/buttons/SimpleButton";
 import PostingInput from "../common/input/PostingInput";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useEffect } from "react";
+
+export interface PostingFormValues {
+  title: string;
+  description: string;
+  tags: string;
+}
 
 const PostingForm = () => {
-  return (
-    <form className=" flex flex-col gap-4 mt-4">
-      <PostingInput placeholder="제목을 입력해주세요" />
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostingFormValues>();
 
-      <textarea
-        name=""
-        id=""
-        placeholder="내용을 입력해주세요"
-        className="w-[448px] h-[200px] rounded-[12px] bg-[#f2f5f2] px-4 py-3 outline-none block resize-none"
-      ></textarea>
+  const onSubmit: SubmitHandler<PostingFormValues> = (data) => {
+    console.log(data);
+  };
+
+  useEffect(() => {
+    console.log(errors);
+  }, [errors]);
+
+  return (
+    <form
+      className=" flex flex-col gap-4 mt-4"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <PostingInput
+        placeholder="제목을 입력해주세요"
+        register={register}
+        label="title"
+        errors={errors}
+        required
+      />
+
+      <div>
+        <label htmlFor="description" className="font-bold mb-1 block">
+          내용
+        </label>
+        <textarea
+          placeholder="내용을 입력해주세요"
+          className={twMerge(
+            "w-[448px] h-[200px] rounded-[12px] bg-[#f2f5f2] px-4 py-3 outline-none block resize-none",
+            errors.description ? "border border-red-500" : ""
+          )}
+          {...register("description", { required: "내용을 입력해주세요" })}
+        ></textarea>
+        {errors.description && (
+          <p className="text-red-500 text-[12px]">
+            {errors.description.message}
+          </p>
+        )}
+      </div>
 
       <div className="w-full h-[232px] flex flex-col items-center justify-center gap-3 border border-dashed border-[#dee3db] rounded-[12px]">
         <p className="text-[18px] font-bold text-[#121712]">이미지 추가하기</p>
@@ -30,10 +76,18 @@ const PostingForm = () => {
         />
       </div>
 
-      <PostingInput placeholder="태그를 쉼표로 구분하여 입력해주세요" />
+      <PostingInput
+        placeholder="태그를 쉼표로 구분하여 입력해주세요"
+        register={register}
+        label="tags"
+        required
+        errors={errors}
+      />
 
       <div className="flex justify-end">
-        <SimpleButton css="w-[84px] h-[40px] bg-[#e8f6e8]">올리기</SimpleButton>
+        <SimpleButton css="w-[84px] h-[40px] bg-[#e8f6e8]" type="submit">
+          올리기
+        </SimpleButton>
       </div>
     </form>
   );
