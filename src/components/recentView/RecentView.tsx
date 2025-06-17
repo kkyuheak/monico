a6ceedf9bcb60c9271c5f11351ab2dc753ba62fa
kkyuheak <gyuheak@gmail.com>
2025-06-17 23:54:56 +0900

    feat: 게시글 등록 로딩 애니메이션 추가

--- src/components/recentView/RecentView.tsx
+++ src/components/recentView/RecentView.tsx
@@ -50,9 +50,9 @@ const RecentView = () => {
   }, []);
 
   return (
-    <>
-      <div className="w-[120px] min-h-[330px] bg-[#fafafa] shadow opacity-95 fixed right-5 bottom-5 rounded-lg p-3 flex flex-col max-lg:hidden">
-        <h1 className="text-[17px] text-center font-semibold">최근 본 코인</h1>
+    <div className="w-[120px] min-h-[330px] bg-[#fafafa] shadow opacity-95 fixed right-5 bottom-5 rounded-lg p-3 flex flex-col max-lg:hidden">
+      <h1 className="text-[17px] text-center font-semibold">최근 본 코인</h1>
+      {recentCoinData.length > 0 ? (
         <ul className="flex flex-col gap-4 items-center mt-4">
           {recentCoinData.map((coin, index) => {
             const coinSymbol = coin.market.split("-")[1];
@@ -75,23 +75,12 @@ const RecentView = () => {
             );
           })}
         </ul>
-      </div>
-
-      {/* 1024px 이하 */}
-      <div
-        className="max-lg:flex lg:hidden w-10 hover:w-[120px] h-10 bg-gray-100 border border-gray-500 rounded-full fixed right-5 bottom-5 shadow cursor-pointer transition-all duration-300 ease-in-out flex items-center justify-center overflow-hidden group"
-        onClick={() => router.push("/recent")}
-      >
-        <p className="text-[15px] font-semibold whitespace-nowrap transition-all duration-300">
-          <span className="opacity-100 group-hover:opacity-0 inline-block group-hover:hidden">
-            최근
-          </span>
-          <span className="hidden group-hover:inline-block opacity-0 group-hover:opacity-100">
-            최근 본 코인
-          </span>
+      ) : (
+        <p className="text-center text-[#6e8566] font-semibold text-[15px] flex items-center justify-center mt-4">
+          최근 본 코인이 없습니다.
         </p>
-      </div>
-    </>
+      )}
+    </div>
   );
 };
 
