d150c7dbd98aba0bdf5b87a63aaa9e52f7c70928
kkyuheak <gyuheak@gmail.com>
2025-06-20 19:19:55 +0900

    feat: 뉴스룸 페이지 다크모드 구현

--- src/app/news/layout.tsx
+++ src/app/news/layout.tsx
@@ -2,7 +2,7 @@ const NewsPageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="max-w-[960px] m-auto py-2">
       <h1 className="text-[28px] font-bold mt-4">뉴스룸</h1>
-      <p className="text-[16px] font-medium text-[#6e8566] my-2">
+      <p className="text-[16px] font-medium text-[#6e8566] dark:text-[#9CABBA] my-2">
         최신 뉴스를 확인할 수 있습니다.
       </p>
       {children}
--- src/components/skeleton/CoinRecommendBoxSkeleton.tsx
+++ src/components/skeleton/CoinRecommendBoxSkeleton.tsx
@@ -1,19 +1,19 @@
 const CoinRecommendBoxSkeleton = () => {
   return (
-    <div className="w-[250px] h-[150px] bg-[#f3f3f3] rounded-md flex flex-col gap-3 justify-center px-3 py-2 shadow animate-pulse">
+    <div className="w-[250px] h-[150px] bg-[#f3f3f3] dark:bg-gray-600 rounded-md flex flex-col gap-3 justify-center px-3 py-2 shadow animate-pulse">
       <div className="flex items-center gap-2">
-        <div className="rounded-full bg-gray-300 w-[35px] h-[35px]"></div>
+        <div className="rounded-full bg-gray-300 dark:bg-gray-600 w-[35px] h-[35px]"></div>
         <div className="flex flex-col justify-center w-[150px]">
-          <div className="bg-gray-300 h-4 w-3/4 rounded"></div>
-          <div className="bg-gray-300 h-3 w-1/2 rounded mt-1"></div>
+          <div className="bg-gray-300 dark:bg-gray-600 h-4 w-3/4 rounded"></div>
+          <div className="bg-gray-300 dark:bg-gray-600 h-3 w-1/2 rounded mt-1"></div>
         </div>
       </div>
       <div className="flex items-baseline gap-1 mt-2">
-        <div className="bg-gray-300 h-6 w-1/3 rounded"></div>
-        <div className="bg-gray-300 h-4 w-1/4 rounded"></div>
+        <div className="bg-gray-300 dark:bg-gray-600 h-6 w-1/3 rounded"></div>
+        <div className="bg-gray-300 dark:bg-gray-600 h-4 w-1/4 rounded"></div>
       </div>
       <div className="flex items-center mt-2">
-        <div className="bg-gray-300 h-4 w-1/4 rounded"></div>
+        <div className="bg-gray-300 dark:bg-gray-600 h-4 w-1/4 rounded"></div>
       </div>
     </div>
   );
