9599fa3681aa08d63e2545d1dce2543307a9c267
kkyuheak <gyuheak@gmail.com>
2025-07-29 23:47:50 +0900

    feat: 코인 메인페이지 반응형 디자인 구현

--- src/components/coin/CoinListSkeleton.tsx
+++ src/components/coin/CoinListSkeleton.tsx
@@ -1,9 +1,9 @@
 const CoinListSkeleton = () => {
   return (
-    <tr className="border-b border-[#e8e8e8] dark:border-[#373737] h-[64px] animate-pulse">
-      <td className="pl-3">
+    <tr className="border-b border-[#e8e8e8] dark:border-[#373737] h-[64px] animate-pulse max-md:h-[42px]">
+      <td className="pl-3 max-md:pl-2">
         <div className="flex h-full items-center gap-3">
-          <div className="w-[30px] h-[30px] rounded-full bg-gray-300 dark:bg-gray-600" />
+          <div className="w-[30px] h-[30px] rounded-full bg-gray-300 dark:bg-gray-600 max-md:hidden" />
           <div className="w-[80px] h-[20px] bg-gray-300 dark:bg-gray-600 rounded-md" />
         </div>
       </td>
@@ -13,7 +13,7 @@ const CoinListSkeleton = () => {
       <td className="text-center">
         <div className="w-[50px] h-[20px] bg-gray-300 dark:bg-gray-600 mx-auto rounded-md" />
       </td>
-      <td className="text-center">
+      <td className="text-center max-md:hidden">
         <div className="w-[80px] h-[20px] bg-gray-300 dark:bg-gray-600 mx-auto rounded-md" />
       </td>
       <td className="text-center">
--- src/components/common/buttons/SearchBarButton.tsx
+++ src/components/common/buttons/SearchBarButton.tsx
@@ -7,11 +7,11 @@ interface SearchBarButtonProps {
 const SearchBarButton = ({ onClick }: SearchBarButtonProps) => {
   return (
     <button
-      className="w-[200px] h-[35px] rounded-[10px] px-2 py-1 border outline-none text-left flex items-center gap-2 text-[12px] cursor-pointer"
+      className="w-[200px] max-md:w-full h-[35px] rounded-[10px] px-2 py-1 border outline-none text-left flex items-center gap-2 text-[12px] cursor-pointer"
       onClick={onClick}
     >
       <Search stroke="gray" width={20} height={20} />
-      검색
+      <span className="">검색</span>
     </button>
   );
 };
