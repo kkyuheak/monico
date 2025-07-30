5cb31a7e11298721d447b46942d678af570a076d
kkyuheak <gyuheak@gmail.com>
2025-07-30 23:18:26 +0900

    feat: 코인 상승/하락 모바일 Top5 스켈레톤 디자인 적용

--- src/components/coin/mobile/MobileTopCoin.tsx
+++ src/components/coin/mobile/MobileTopCoin.tsx
@@ -1,3 +1,4 @@
+import MobileTopCoinItemSkeleton from "@/components/skeleton/MobileTopCoinItemSkeleton";
 import MobileTopCoinItem from "./MobileTopCoinItem";
 
 interface MobileTopCoinProps {
@@ -7,27 +8,29 @@ interface MobileTopCoinProps {
 }
 
 const MobileTopCoin = ({ top5Coin, type, coinName }: MobileTopCoinProps) => {
-  console.log(top5Coin);
-
   return (
     <div className="w-full h-[260px] p-1 bg-gray-50 dark:bg-gray-800 flex flex-col gap-2 rounded-lg">
-      {top5Coin?.map((coin) => {
-        const kr_name = coinName?.find(
-          (nameData) => nameData.market === coin.market
-        );
+      {top5Coin
+        ? top5Coin.map((coin) => {
+            const kr_name = coinName?.find(
+              (nameData) => nameData.market === coin.market
+            );
 
-        return (
-          <MobileTopCoinItem
-            key={coin.market}
-            type={type}
-            coinName={coin.market}
-            coinKrName={kr_name?.korean_name ?? ""}
-            coinPrice={coin.trade_price}
-            coinChangeRate={coin.change_rate}
-            coinChange={coin.change}
-          />
-        );
-      })}
+            return (
+              <MobileTopCoinItem
+                key={coin.market}
+                type={type}
+                coinName={coin.market}
+                coinKrName={kr_name?.korean_name ?? ""}
+                coinPrice={coin.trade_price}
+                coinChangeRate={coin.change_rate}
+                coinChange={coin.change}
+              />
+            );
+          })
+        : Array.from({ length: 5 }).map((_, i) => (
+            <MobileTopCoinItemSkeleton key={i} />
+          ))}
     </div>
   );
 };
